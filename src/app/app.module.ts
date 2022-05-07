6f080597aef9a33298787e47e3684c133bd4e71c
Azeddine Assoumati <assoumati95azeddine@gmail.com>
2022-05-07 13:45:52 +0000

    install Angular/material -- setup home&not-found components -- add global style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { AppComponent } from "./app.component";
 import { appRoutes } from "./app.routes";
-import { HomeComponent } from "./components/home.component";
-import { NotFoundComponent } from "./components/not-found.component";
+import { HomeComponent } from "./components/home/home.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from "./modules/shared/shared.module";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, appRoutes, SharedModule],
+  imports: [BrowserModule, SharedModule, appRoutes, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from "@angular/core";
 import { BackendService } from "src/app/services/backend.service";
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   declarations: [],
-  exports: [],
+  exports: [
+    MatIconModule
+  ],
   providers: [BackendService],
 })
-export class SharedModule {}
+export class SharedModule { }
